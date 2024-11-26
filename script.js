237e66cd3fc5c89aed3740a15cdae918c0bc964b
Nathan Carter <nathanlawcarter@gmail.com>
2024-11-26 13:15:23 -0500

    small changes

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Wait for the DOM to load before executing JavaScript
 document.addEventListener('DOMContentLoaded', () => {
   // Initialize the map and set its view to a geographical point and zoom level
-  const map = L.map('map').setView([51.505, -0.09], 13);
+  const map = L.map('map').setView([43.599787, -79.64447], 13);
 
   // Add the OpenStreetMap tiles to the map
   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -23,19 +23,52 @@ document.addEventListener('DOMContentLoaded', () => {
   //   radius: 500
   // }).addTo(map);
 
-  // // Add a polygon to the map
-  // const polygon = L.polygon([
-  //   [51.509, -0.08],
-  //   [51.503, -0.06],
-  //   [51.51, -0.047]
-  // ]).addTo(map);
+  // Add a polygon to the map
+  const High_Building_Footprint = L.polygon(
+      [
+        [43.558625, -79.707104],
+        [43.535483, -79.73321],
+        [43.582505, -79.79727],
+        [43.595809, -79.782303],
+        [43.598295, -79.763411]
+      ],
+      {
+        color: 'black',        // Outline color
+        fillColor: 'black',  // Fill color
+        fillOpacity: 0.5,     // Fill transparency
+        weight: 3             // Outline thickness
+      }
+    ).addTo(map);
+
+    // Add a polygon to the map
+  const Natural_Area = L.polygon(
+      [
+        [43.559807, -79.61678],
+        [43.556324, -79.620859],
+        [43.554613, -79.614462],
+        [43.558034, -79.61064],
+        [43.557381, -79.613431]
+      ],
+      {
+        color: 'Green',        // Outline color
+        fillColor: 'Green',  // Fill color
+        fillOpacity: 0.5,     // Fill transparency
+        weight: 3             // Outline thickness
+      }
+    ).addTo(map);
 
   // // Popup for the circle
   // circle.bindPopup('I am a circle.');
   // polygon.bindPopup('I am a polygon.');
 
-  // // Popup on map click
-  // map.on('click', function (e) {
-  //   alert(`You clicked the map at ${e.latlng}`);
-  // });
+  High_Building_Footprint.bindPopup('I am a zone with a high building footprint! I have low biodiversity,\
+      so I would benefit from green rooftops! My data comes from the Building_Footprints dataset!');
+
+  Natural_Area.bindPopup('I am natural zone sanctioned by the city! I already am very green, so I do not really need \
+      any green rooftops nearby. Instead cheaper white or blue rooftops can be installed to save money. My data comes from the Natural_Area_System_(NAS) Dataset');
+
+  // Popup on map click
+  map.on('click', function (e) {
+      alert(`You clicked the map at ${e.latlng}`);
+  });
 });
